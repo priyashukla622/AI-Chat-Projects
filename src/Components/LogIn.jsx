2edd7f8a08edcbabfa365a66bc6d40b4d00eaaca
AlkaTiwari2002 <alkat5359@gmail.com>
2025-04-02 12:06:08 +0530

    Updated files

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,51 +1,3 @@
-// import React, { useState } from "react";
-// import "./Form.css";
-
-// const Login = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.log("Email:", email);
-//     console.log("Password:", password);
-//     alert("Login Successful!");
-
-//     setEmail("");        
-//     setPassword("");
-//   };
-
-//   return (
-//     <div className="container">
-//       <form onSubmit={handleSubmit} className="logIn-form">
-//         <h2>LogIn Form</h2>
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//           required
-//           className="input"
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//           required
-//           className="input"
-//         />
-//         <button type="submit" className="button">Login</button>
-//       </form>
-//     </div>
-//   );
-// };
-// export default Login;
-
-
-
-
-
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Form.css";
@@ -82,7 +34,6 @@ const Login = () => {
       alert("Something went wrong. Please try again.");
     }
 
-    // Reset the form fields
     setEmail("");
     setPassword("");
   };
