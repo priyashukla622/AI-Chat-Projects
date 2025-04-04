0da79e711785fdea575d05ddc02ead499859b05f
priya shukla <priyashukla22@navgurukul.org>
2025-04-04 23:54:35 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-// import WelcomePage from "./Components/Welcome";
-// import UiPage from "./Components/UiPage";
-// import Login from "./Components/LogIn";
-// import SignUp from "./Components/SignUp";
-// function App() {
-//   return (
-//     <>
-//     <Router>
-//       <Routes>
-//       <Route path="/" element={<WelcomePage />} />  
-//       <Route path="/ui" element={<UiPage />} />  
-//         <Route path="/signUp" element={<SignUp />} />
-//         <Route path="/login" element={<Login />} />
-//       </Routes>
-//     </Router>
-//     </>
-//   );
-// }
-// export default App;
-
-
-
-
-
-
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import WelcomePage from "./Components/Welcome";
